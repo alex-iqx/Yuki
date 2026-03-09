0043f384123aba850f133c9d1d439a7c61ceac4e
DannyNghtmr <nnjd@hz3Nxedv9P>
2026-03-09 16:54:33 +0200

    Updated prefix

--- Util/constants.js
+++ Util/constants.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 module.exports = {
-  PREFIX: process.env.PREFIX || '-',
+  PREFIX: process.env.PREFIX || '+',
   ALLOWED_ROLE_ID: process.env.ALLOWED_ROLE_ID,
   BOT_TOKEN: process.env.BOT_TOKEN,
   APPLICATION_ID: process.env.APPLICATION_ID
